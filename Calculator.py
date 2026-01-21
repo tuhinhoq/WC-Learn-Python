9d5d6afb8756189e394576c538b7feeef497c218
TuHiN HoQuE <104512309+tuhinhoq@users.noreply.github.com>
2026-01-21 11:23:22 +0600

    Calculator_Day_1

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+print("Enter a number: ")
+x=input()
+z=int(x)
+
+print("Enter another number: ")
+y=input()
+zz=int(y)
+
+add=z+zz
+sub=z-zz
+mul=z*zz
+div=z/zz
+
+print("A= ",z, "&", "B= ",zz)
+print("Result Of Sum: ",add)
+print("Result Of Sub: ",sub)
+print("Result Of Mul: ",mul)
+print("Result Of Div: ",div)
