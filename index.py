f974cb8eed4301bee2a5d259a2ae04f555cdc7df
TuHiN HoQuE <104512309+tuhinhoq@users.noreply.github.com>
2026-01-21 10:50:19 +0600

    Day-1

---
+++ index.py
@@ -0,0 +1,34 @@
+a=10
+print(a)
+print(type(a))
+b=10.10
+print(b)
+print(type(b))
+c=True
+print(c)
+print(type(c))
+d="Python"
+print(d)
+print(type(d))
+
+
+aa=10
+bb=20
+cc=aa+bb
+dd=aa-bb
+ee=aa*bb
+ff=aa/bb
+print( "a=10 & b=20")
+print("a+b = ",cc)
+print("a-b = ",dd)
+print("a*b = ",ee)
+print("a/b = ",ff)
+
+ac=10
+print(type(ac))
+print("After Covert Int to Float :",float(ac),type(ac))
+
+
+do=10.20
+print(type(do))
+print("After Covert Float to Int :",int(do),type(do))
